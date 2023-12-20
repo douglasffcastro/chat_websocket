42cf2cb13e35003567d52c3e146d4993e554244f
Sebastian Zuzunaga <seb55381@hotmail.com>
2023-12-20 17:32:16 -0500

    add:adicioada tela de login/precisa de revisao

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, join_room
+from flask_login import current_user, login_user, login_required, logout_user, LoginManager
 
 app = Flask(__name__)
+app.secret_key = "sfdjkafnk"
 socketio = SocketIO(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+        user = get_user(username)
+
+        if user and user.check_password(password_input):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            message = 'Failed to login!'
+    return render_template('login.html', message=message)
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/chat')
 def chat():
@@ -17,6 +46,7 @@ def chat():
         return render_template("chat_T.html")
     else:
         return redirect(url_for('home'))
+    
 
 @socketio.on('send_message')
 def hendle_send_message_event(data):
@@ -29,6 +59,16 @@ def handle_join_room_event(data):
     app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
     join_room(data['room'])
     socketio.emit('join_room_anouncement', data)
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    app.logger.info("{} has left the room {}".format(data['username'], data['room']))
+    leave_room(data['room'])
+    socketio.emit('leave_room_announcement', data, room=data['room'])
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
